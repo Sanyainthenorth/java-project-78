0bb2d2934cf3bbbf7c0957ba5e4ae773454db00e
Александра Яицкая <alexandraaaaa77777@mail.ru>
2025-06-02 19:00:29 +0300

    изменен StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,29 +1,30 @@
 package hexlet.code.schemas;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public final class StringSchema extends BaseSchema<String> {
 
-    private final List<Predicate<String>> checks = new ArrayList<>();
+    private final List<Predicate<String>> rules = new ArrayList<>();
 
     @Override
     public StringSchema required() {
         this.isRequired = true;
-        checks.add(value -> value != null && !value.isEmpty());
+        this.rules.clear();
+        rules.add(Objects::nonNull);
+        rules.add(value -> !value.isEmpty());
         return this;
     }
 
     public StringSchema minLength(int length) {
-        checks.add(value -> value.length() >= length);
+        rules.add(value -> value == null || value.length() >= length);
         return this;
     }
 
     public StringSchema contains(String substring) {
-        checks.add(value -> value.contains(substring));
+        rules.add(value -> value == null || value.contains(substring));
         return this;
     }
 
@@ -32,15 +33,18 @@ public final class StringSchema extends BaseSchema<String> {
         if (value == null) {
             return !isRequired;
         }
-
         if (!(value instanceof String)) {
             return false;
         }
 
         String stringValue = (String) value;
 
-        for (Predicate<String> check : checks) {
-            if (!check.test(stringValue)) {
+        if (rules.isEmpty()) {
+            return true;
+        }
+
+        for (Predicate<String> rule : rules) {
+            if (!rule.test(stringValue)) {
                 return false;
             }
         }
